4fd775b3bb8f917841f6df7d99e269885fe78d31
eintyp <vonflocken@duck.com>
2024-05-19 14:59:20 +0200

    add functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,18 @@
 <script>
-  import ArrowUp from "./lib/icons/ArrowUp.svelte";
-  import ArrowDown from "./lib/icons/ArrowDown.svelte";
+  import Spinner from './lib/Spinner.svelte'
+
+  const path_expand = 'M10,6L16,0M11,0H16V5M6,10L0,16M0,11V16H5'
+  const path_collapse = 'M0,16L6,10M1,10H6V15M16,0L10,6M10,1V6H15'
+  // weird but works
+  let fullscreen_element = document.fullscreenElement
+  document.addEventListener('fullscreenchange', () => fullscreen_element = document.fullscreenElement)
+  $: fullscreen_path = fullscreen_element ? path_collapse : path_expand
+
+  function toggle_fullscreen() {
+    if (!fullscreen_element) document.documentElement.requestFullscreen()
+    else document.exitFullscreen()
+  }
+
 
   let hours = 0, minutes = 10, seconds = 0
   $: hours_formatted = `${hours}`.padStart(2, '0')
@@ -30,20 +42,45 @@
   $: if (hours < 0) hours = 0
   $: if (hours > 99) hours = 99
 
-  //setInterval(()=>seconds--, 100)
+  //setInterval(()=>seconds--, 1000)
 
-  let secondary_text = 'löschen', primary_text = 'start'
+  let secondary = { text: 'abbrechen', action: reset }, primary = { text: 'start', action: start }
 
-  const path_expand = 'M10,6L16,0M11,0H16V5M6,10L0,16M0,11V16H5'
-  const path_collapse = 'M0,16L6,10M1,10H6V15M16,0L10,6M10,1V6H15'
-  // weird but works
-  let fullscreen_element = document.fullscreenElement
-  document.addEventListener('fullscreenchange', () => fullscreen_element = document.fullscreenElement)
-  $: fullscreen_path = fullscreen_element ? path_collapse : path_expand
+  let finish_audio, interval, state = 'initial', finished = false
+  function start() {
+    interval = setInterval(tick, 1000)
+    primary.text = 'pause'
+    primary.action = pause
+    state = 'running'
+    if (!finish_audio) finish_audio = new Audio('https://gcm.schule/timer_over.wav')
+  }
+  function pause() {
+    clearInterval(interval)
+    primary.text = 'weiter'
+    primary.action = start
+    state = 'paused'
+  }
+  function reset() {
+    pause()
+    set(0, 10, 0)
+    primary.text = 'start'
+    state = 'initial'
+    finished = false
+  }
 
-  function toggle_fullscreen() {
-    if (!fullscreen_element) document.documentElement.requestFullscreen()
-    else document.exitFullscreen()
+  function tick() {
+    if (finished) seconds ++
+    else seconds --
+    if (hours + minutes + seconds === 0) {
+      finish_audio.play()
+      finished = true
+      primary.text = 'fertig'
+      primary.action = reset
+    }
+  }
+
+  function set(h, m, s) {
+    hours = h; minutes = m; seconds = s
   }
 </script>
 
@@ -60,20 +97,26 @@
   <svg viewBox="-2 -2 20 20" height="24" fill="none" stroke="currentColor"><path d={fullscreen_path}/></svg>
 </button>
 
-<audio src="https://gcm.schule/public/timer_over.wav" id="finish-audio"></audio>
-
 <main class="h-screen flex flex-col items-center justify-center">
   <div class="grid grid-cols-3">
-    <button class="flex justify-center z-10 rounded-md hover:bg-gray-200" on:click={()=>hours++}><ArrowUp/></button>
-    <button class="flex justify-center z-10 rounded-md hover:bg-gray-200" on:click={()=>minutes++}><ArrowUp/></button>
-    <button class="flex justify-center z-10 rounded-md hover:bg-gray-200" on:click={()=>seconds++}><ArrowUp/></button>
-    <div class="text-5xl sm:text-9xl tabular-nums col-span-3">{ hours_formatted }:{ minutes_formatted }:{ seconds_formatted }</div>
-    <button class="flex justify-center z-10 rounded-md hover:bg-gray-200" on:click={()=>hours--}><ArrowDown/></button>
-    <button class="flex justify-center z-10 rounded-md hover:bg-gray-200" on:click={()=>minutes--}><ArrowDown/></button>
-    <button class="flex justify-center z-10 rounded-md hover:bg-gray-200" on:click={()=>seconds--}><ArrowDown/></button>
+    <Spinner direction="up" hidden={state === 'running'} on:click={()=>hours++} />
+    <Spinner direction="up" hidden={state === 'running'} on:click={()=>minutes++} />
+    <Spinner direction="up" hidden={state === 'running'} on:click={()=>seconds++} />
+    <div class="text-5xl sm:text-9xl tabular-nums col-span-3" class:pulse={finished}>{ hours_formatted }:{ minutes_formatted }:{ seconds_formatted }</div>
+    <Spinner direction="down" hidden={state === 'running'} on:click={()=>hours--} />
+    <Spinner direction="down" hidden={state === 'running'} on:click={()=>minutes--} />
+    <Spinner direction="down" hidden={state === 'running'} on:click={()=>seconds--} />
     <div class="flex col-span-3 mt-8 -mb-14">
-      <button class="grow basis-0 font-bold rounded-md px-4 hover:bg-gray-200">{ secondary_text }</button>
-      <button class="grow basis-0 font-bold rounded-md px-4 hover:bg-gray-200 text-green">{ primary_text }</button>
+      {#if state === 'initial'}
+        <div class="grow basis-0">
+          {#each [5,10,15,20,25,30,45,60] as t}
+            <button class="font-bold rounded-md px-2 -mr-1 hover:bg-gray-200 relative hover:z-10 max-sm:hidden" on:click={() => set(0, t, 0)}>{t}</button>
+          {/each}
+        </div>
+      {:else}
+        <button class="grow basis-0 box-border font-bold rounded-md hover:bg-gray-200" on:click={secondary.action}>{ secondary.text }</button>
+      {/if}
+      <button class="grow basis-0 box-border font-bold rounded-md hover:bg-gray-200 text-green" on:click={primary.action}>{ primary.text }</button>
     </div>
   </div>
 </main>
---
+++ src/lib/Spinner.svelte
@@ -0,0 +1,20 @@
+<script>
+    import ArrowUp from './icons/ArrowUp.svelte'
+    import ArrowDown from './icons/ArrowDown.svelte'
+    import {createEventDispatcher} from 'svelte'
+
+    export let direction
+    export let hidden = false
+
+    const dispatch = createEventDispatcher()
+</script>
+
+<button class="flex justify-center z-10 rounded-md hover:bg-gray-200"
+        class:invisible={hidden}
+        on:click={e => dispatch('click', e)}>
+    {#if direction === 'up'}
+        <ArrowUp />
+    {:else}
+        <ArrowDown />
+    {/if}
+</button>
